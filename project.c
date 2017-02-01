ff1b9916e3154139dbf952d8d1da00cb88c92dab
the2ew <ctewthe2@yahoo.com>
2017-02-01 09:28:00 -0700

    Add files via upload

---
+++ project.c
@@ -0,0 +1,14 @@
+/* 1 v 1 battle game. 
+player sheet.
+-Create character (struct, FILE* & for loops)
+-Allocate stats 
+New/load option (if statements, FILE*)
+Attack interface
+-options (if/while statments)
+Take info from character sheet to pit two characters against eachother. 
+randome die roll for winner. (Probability?)
+Score board/statistics page.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
